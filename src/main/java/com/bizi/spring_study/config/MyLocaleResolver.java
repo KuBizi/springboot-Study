cfe5e56f1a584529378c120f3fa71f322901529e
fujl01 <fujl01@mingyuanyun.com>
2022-06-03 20:39:23 +0800

    国际化配置类

---
+++ src/main/java/com/bizi/spring_study/config/MyLocaleResolver.java
@@ -0,0 +1,32 @@
+package com.bizi.spring_study.config;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        // 获取请求域中的语言参数信息
+        String lanuage = request.getParameter("");
+        
+        // 获取默认语言参数
+        Locale aDefault = Locale.getDefault();
+        
+        if (StringUtils.isNotEmpty(lanuage)) {
+            // zh_CN  语言文化代码和国家地区 zh-HK 华 - 香港的 SARzh-MO 华 - 澳门的 SARzh-CHS 华 (单一化)zh-SG 华 -新加坡zh-TW 华 -台湾zh-CHT 华
+            String[] split = lanuage.split("_");
+            aDefault = new Locale(split[0],split[1]);
+        }
+
+        return aDefault;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+
+    }
+}
